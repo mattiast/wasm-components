9fd89d732adaf6a3f86d75eb7741748b5f4f3508
Matti Åstrand <mattiastr@gmail.com>
2025-12-24 13:31:41 -0500

    Rust wit bindings

---
+++ sos/src/lib.rs
@@ -0,0 +1,14 @@
+use wit_bindgen::generate;
+
+generate!();
+
+pub struct Thing;
+impl Guest for Thing {
+    fn sum_of_squares(x: u32, y: u32) -> u32 {
+        let x2 = square(x);
+        let y2 = square(y);
+        x2 + y2
+    }
+}
+
+export!(Thing);
